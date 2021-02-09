0d637605c8f3eae7875dbd3479eb61aa61579c24
Julian Patterson <julian.patterson@gmail.com>
2021-02-09 18:00:00 -0500

    commit tree 8bf73014604695843453c023d06f5f78c62184f1

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names) # code an argument here
   # Your code here
+  names.each.with_index(1) {|name, index| puts "#{index}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.collect {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(possible_cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  possible_cheeses.find {|cheese| cheese_types.include?(cheese)}
 end
